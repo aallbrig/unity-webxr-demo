82a660c11d0e082c4a7f006863c11a7da9a5717d
aallbrig <aallbrig@gmail.com>
2022-02-24 22:27:41 +0000

    Deploying to github-pages from @ aallbrig/webgl-vr-demo@ad07bb97140589f6268efa9d78c7e1d23ed92be8 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "WebGL VR demo",
-    productVersion: "0.0.9",
+    productVersion: "0.0.10",
     devicePixelRatio: 1,
 }
 
