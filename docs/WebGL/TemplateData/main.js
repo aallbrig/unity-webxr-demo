e5680272b96ea2cf94867c5e67016b3c67341061
aallbrig <aallbrig@gmail.com>
2022-02-24 03:52:44 +0000

    Deploying to github-pages from @ aallbrig/webgl-vr-demo@71f5197cd772db4b82de648ff927f8ffaf4e08e7 🚀

---
+++ docs/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Andrew Allbright",
+    productName: "WebGL VR demo",
+    productVersion: "0.0.4",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
