a0bdd411ca76a07068b8936e4be844d8aebe66e8
aallbrig <aallbrig@gmail.com>
2022-02-26 17:05:44 +0000

    Deploying to github-pages from @ aallbrig/webgl-vr-demo@3016c693b26bebf5ff04c52b6c04fb833242db2b 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "WebGL VR demo",
-    productVersion: "0.0.17",
+    productVersion: "0.0.18",
     devicePixelRatio: 1,
 }
 
--- docs/WebGL/TemplateData/webxr.js
+++ docs/WebGL/TemplateData/webxr.js
@@ -1,3 +1,32 @@
 class Webxr {
-  
+  arEnabled = false;
+  vrEnabled = false;
+  DoesNavigatorXRExists() {
+    return window.navigator.xr ? true: false;
+  }
+
+  async DetermineWebXRStatus() {
+    var arEnabled = this.DetermineARStatus();
+    var vrEnabled = this.DetermineVRStatus();
+    this.arEnabled = await arEnabled;
+    this.vrEnabled = await vrEnabled;
+  }
+
+  toString() {
+    return `AR enabled: ${this.arEnabled}\nVR enabled: ${this.vrEnabled}`;
+  }
+
+  async DetermineARStatus() {
+    if (!this.DoesNavigatorXRExists()) {
+      return false;
+    }
+    return await window.navigator.xr.isSessionSupported('immersive-ar');
+  }
+
+  async DetermineVRStatus() {
+    if (!this.DoesNavigatorXRExists()) {
+      return false;
+    }
+    return await window.navigator.xr.isSessionSupported('immersive-vr');
+  }
 }
