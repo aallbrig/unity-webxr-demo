5682454e6df74d25295ca674bd96d34cf50f021c
aallbrig <aallbrig@gmail.com>
2022-03-01 01:23:32 +0000

    Deploying to github-pages from @ aallbrig/unity-webxr-demo@abce9f5b6ceee521997a55b33a843e88452c8f01 🚀

--- docs/WebGL/TemplateData/main.js
+++ docs/WebGL/TemplateData/main.js
@@ -7,7 +7,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Andrew Allbright",
     productName: "WebGL VR demo",
-    productVersion: "0.0.28",
+    productVersion: "0.0.29",
     devicePixelRatio: 1,
 }
 
